18d704f204be536b4cf45d3c02d0d7f189d8cb52
zsumore <zsumore@163.com>
2013-12-18 16:35:56 +0800

    init2

---
+++ restful-core/src/main/java/org/restsql/core/SqlResource.java
@@ -0,0 +1,34 @@
+/* Copyright (c) restSQL Project Contributors. Licensed under MIT. */
+package org.restsql.core;
+
+import org.restsql.core.sqlresource.SqlResourceDefinition;
+
+/**
+ * Represents an SQL Resource, a queryable and updatable database "view".
+ * 
+ * @author Mark Sawers
+ */
+public interface SqlResource {
+	/**
+	 * Returns SQL resource information defined by the user, including query, validated attributes and trigger.
+	 * 
+	 * @return definition
+	 */
+	public SqlResourceDefinition getDefinition();
+
+	/**
+	 * Returns SQL resource name.
+	 * 
+	 * @return SQL resource name
+	 */
+	public String getName();
+
+	/**
+	 * Returns meta data for SQL resource.
+	 * 
+	 * @return SQL rsource meta data
+	 */
+	public SqlResourceMetaData getMetaData();
+
+	
+}
